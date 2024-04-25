f9875668c2c2ed003b69fd9668ab66e547a9305f
SSSBoOm <boom1909chanapat@gmail.com>
2024-04-25 11:46:28 +0700

    chore: add color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#042330',
+        'primary-orange': '#F89200',
+        'primary-yellow': '#FFD234',
+      },
+    },
   },
   plugins: [],
 }
