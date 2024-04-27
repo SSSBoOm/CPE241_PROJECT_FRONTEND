dd942d68817fe3f446100a4555ecdcc46f6da45d
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2024-04-27 09:25:33 +0700

    feat.(RoomCardUpdate)

--- src/components/Card/RoomCard.tsx
+++ src/components/Card/RoomCard.tsx
@@ -2,26 +2,26 @@ import { ExpandAltOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
 
 type Props = {
-  name: string
+  name?: string
   content?: string
   size?: string
   accommodate?: number
-  food?: boolean
+  food: boolean
   view: {
     sea: boolean
     forest: boolean
   }
   image?: string
-  price: number
+  price?: number
 }
 
 const RoomCard = (props: Props) => {
   return (
-    <div className=" grid grid-cols-3 box-content h-64 w-[52rem] border-4 text-primary-blue-600 content-center ">
-      <div className="w-full col-span-1 items-center content-center last:">
-        <img src={props.image} className=" w-full " alt="" />
+    <div className=" container grid grid-cols-3 box-content h-72 w-[52rem] border-4 text-primary-blue-600 content-center ">
+      <div className="w-full col-span-1 items-center content-center flex ">
+        <img src={props.image} className=" w-4/6 " alt="" />
       </div>
-      <div className=" w-full col-span-2 row-span-1 p-3 grid grid-cols-2 grid-rows-8">
+      <div className=" w-full col-span-2 row-span-1 p-3 grid grid-cols-2 grid-rows-9">
         <div className=" w-full col-span-2 ">
           <span className=" text-2xl ">{props.name}</span>
         </div>
@@ -32,7 +32,7 @@ const RoomCard = (props: Props) => {
             {props.content}
           </span>
         </div>
-        <div className="w-full row-span-5 col-span-1 grid grid-rows-5">
+        <div className="w-full row-span-6 col-span-1 grid grid-rows-6">
           <div className=" text-base  ">
             <ExpandAltOutlined className=" pr-1"></ExpandAltOutlined>
             content of room
@@ -43,6 +43,7 @@ const RoomCard = (props: Props) => {
           <div>
             <span className=" text-base ">For {props.accommodate}</span>
           </div>
+          <div>{props.food ? <span className=" text-base ">food</span> : null}</div>
           <div>{props.view.sea ? <span className=" text-base ">sea view</span> : null}</div>
           <div>{props.view.forest ? <span className=" text-base ">forest view</span> : null}</div>
         </div>
