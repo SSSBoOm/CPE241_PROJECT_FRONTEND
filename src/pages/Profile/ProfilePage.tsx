3e14359371987ea51eb93666ee1aa6cf0742b513
panachaiiiiiii <112429933+panachaiiiiiii@users.noreply.github.com>
2024-05-07 19:28:37 +0700

    Re

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,77 +1,75 @@
 import { Button, Form, Input } from 'antd'
 import { useState } from 'react'
-const PF = {
-  F_Name: 'Is my name ',
-  L_Name: 'Is My Lastname',
-  Email: 'xxxx@gmail.com',
-  Tell: '0xx-xxx-xxxx',
-  address: '24/2 .....'
-}
+
 const ProfilePage = () => {
   const [componentDisabled, setComponentDisabled] = useState<boolean>(true)
-  const [ChangePassword, setChangPassword] = useState<boolean>(true)
 
   return (
     <>
-      <div className="h-screen content-center bg-[url('hotelview2.svg')] bg-cover bg-repeat">
-        <div className="mx-auto w-2/4  content-center rounded-md bg-white/80">
-          <div className="mx-4 my-6">
-            <p className="mx-16 text-3xl font-bold text-primary-blue-700">User Details</p>
-            <div className=" mx-12 my-4 grid grid-cols-1 rounded-md border-2 border-primary-blue-600 bg-white opacity-100">
-              <Form disabled={componentDisabled} className="my-4">
-                <div className=" mx-6 grid grid-cols-2 gap-3">
-                  <Input defaultValue={PF.F_Name} />
-                  <Input defaultValue={PF.L_Name} />
-                  <Input defaultValue={PF.Email} disabled={true} />
-                  <div></div>
-                  <Input defaultValue={PF.Tell} />
-                  <div></div>
-                  <Input defaultValue={PF.address} />
+      <div className="h-full content-center bg-[url('hotelview2.svg')] bg-cover bg-repeat lg:h-screen">
+        <div className="mx-auto w-5/6 content-center  rounded-md bg-white/80 md:w-5/12">
+          <p className="mx-4 text-3xl font-bold text-primary-blue-700">User Details</p>
+          <div className="container rounded-md border-2 border-primary-blue-600 bg-white md:mx-auto md:my-3 md:w-11/12 ">
+            <Form className="mx-2 my-2" layout="vertical" disabled={componentDisabled}>
+              <div className="grid grid-cols-1 lg:grid-cols-2 lg:text-center">
+                <Form.Item
+                  label="First name"
+                  className="inline-block w-10/12 place-self-center"
+                  rules={[{ required: true }]}
+                >
+                  <Input></Input>
+                </Form.Item>
+
+                <Form.Item
+                  label="Last name"
+                  className="inline-block w-10/12 place-self-center lg:place-content-start"
+                  rules={[{ required: true }]}
+                >
+                  <Input></Input>
+                </Form.Item>
+
+                <Form.Item
+                  label="Email"
+                  className="inline-block w-10/12 place-self-center"
+                  rules={[{ required: true }]}
+                >
+                  <Input disabled={true}></Input>
+                </Form.Item>
+
+                <Form.Item
+                  label="Phone number"
+                  className="inline-block w-10/12 place-self-center lg:place-content-start"
+                  rules={[{ required: true }]}
+                >
+                  <Input></Input>
+                </Form.Item>
+                <Form.Item
+                  label="Address"
+                  className="inline-block w-10/12 place-self-center"
+                  rules={[{ required: true }]}
+                >
+                  <Input></Input>
+                </Form.Item>
+                <Form.Item label=" ">
                   <Button
-                    type="primary"
                     disabled={false}
                     onClick={() => {
                       setComponentDisabled(!componentDisabled)
-                      console.log(PF)
                     }}
+                    type="primary"
+                    className="inline-block w-7/12 place-self-center"
                   >
                     Edit
                   </Button>
-                </div>
-              </Form>
-            </div>
-            <div className="grid grid-cols-2">
-              <Input disabled={ChangePassword}></Input>
-              <Button
-                type="primary"
-                className={ChangePassword == false ? 'hidden' : '' + 'w-fit'}
-                onClick={() => {
-                  setChangPassword(!ChangePassword)
-                }}
-              >
-                เปลี่ยนรหัส
-              </Button>
-              <div className={ChangePassword == true ? 'hidden' : ''}></div>
-              <Input className={ChangePassword == true ? 'hidden' : ''}></Input>
-              <Button
-                type="primary"
-                className={ChangePassword == true ? 'hidden' : '' + 'w-fit'}
-                onClick={() => {
-                  setChangPassword(!ChangePassword)
-                }}
-              >
-                ยืนยัน
-              </Button>
-            </div>
-            <p className="mx-16 text-3xl font-bold text-primary-blue-700">Payment Methods</p>
-            <div className="mx-12 my-4 grid grid-cols-1 rounded-md border-2 border-primary-blue-600 bg-white">
-              <p>Name</p>
-              <p>Name</p>
-              <p>Name</p>
-              <div className="grid grid-cols-5">
-                <p className=" col-span-4">Name</p>
-                <button>button</button>
+                </Form.Item>
               </div>
+            </Form>
+          </div>
+          <p className="mx-4 text-3xl font-bold text-primary-blue-700">Card</p>
+          <div className="container grid h-fit grid-rows-1 gap-3 rounded-md border-2 border-primary-blue-600 bg-white md:mx-auto md:my-3 md:w-11/12">
+            <div className="mx-6 grid grid-cols-9">
+              <p className="col-span-7">Text</p>
+              <Button className="col-span-2">d</Button>
             </div>
           </div>
         </div>
