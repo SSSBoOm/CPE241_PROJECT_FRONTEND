6944e6108d643bed3295451dac84ca8c0539e9ec
panachaiiiiiii <112429933+panachaiiiiiii@users.noreply.github.com>
2024-05-20 15:40:15 +0700

    Login

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,71 +1,48 @@
 import { GoogleOutlined } from '@ant-design/icons'
-import { Button, Card, Form, Input } from 'antd'
+import { Form, Input } from 'antd'
 import React from 'react'
 import { handleSignInWithGoogle } from '../../lib/googleSignUp'
 
-interface LoginFormValues {
-  email: string
-  password: string
-}
 const LoginPage: React.FC = () => {
-  const onFinish = (values: LoginFormValues) => {
-    console.log('Received values:', values)
-  }
-
   return (
-    <div className="flex h-screen items-center justify-center bg-[url('/images/login/view.png')] bg-cover bg-repeat">
-      <Card className="h-2/3 w-1/3 border-2 border-primary-blue-600 opacity-80">
-        <div className="flex h-full flex-col justify-center">
-          <div>
-            <div className="mb-2 text-xl text-primary-blue-600">Meridian Bliss</div>
-            <div className="mb-3 text-4xl font-bold text-primary-blue-600">Login</div>
-          </div>
-          <Form name="login" initialValues={{ remember: true }} onFinish={onFinish}>
-            <div className="mb-4">
-              <div className="mb-2 text-2xl font-bold text-primary-blue-600">Email</div>
-              <Form.Item
-                name="email"
-                rules={[{ required: true, message: 'Please input your email!' }]}
-                className="mb-0"
-              >
-                <Input />
-              </Form.Item>
-            </div>
-            <div className="mb-4">
-              <div className="mb-2 text-2xl font-bold text-primary-blue-600">Password</div>
-              <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-                className="mb-0"
-              >
-                <Input type="password" />
-              </Form.Item>
-            </div>
-            <div className="flex justify-center">
-              <Form.Item className="mb-0">
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="mb-3 h-full min-w-44 text-2xl sm:w-full lg:w-1/3"
-                  style={{ backgroundColor: '#0E4459' }}
-                >
-                  Login
-                </Button>
-              </Form.Item>
-            </div>
-          </Form>
-          <div className="mb-3 mt-2 text-center text-lg font-normal leading-snug text-cyan-900">OR</div>
-          <div className="flex justify-center">
-            <button
-              className="flex h-full min-w-[16rem] flex-row items-center justify-center space-x-4 rounded-md border-2 border-red-700 py-2 text-red-700"
-              onClick={handleSignInWithGoogle}
-            >
-              <GoogleOutlined className="text-3xl" />
-              <h1 className="text-xl">Sign in with Google</h1>
-            </button>
-          </div>
+    <div className="flex h-screen items-center justify-center bg-[url('/images/login/view.png')]  bg-cover bg-repeat">
+      <div className=" h-screen w-screen border-primary-blue-600 bg-white px-10 py-4 md:h-fit md:w-5/12 md:rounded-md md:border-2">
+        <div className="mx-4">
+          <p className="text-xs font-medium text-primary-blue-700 lg:text-lg">Meridian Bliss</p>
+          <p className="text-3xl font-bold text-primary-blue-700 lg:text-5xl">Login</p>
         </div>
-      </Card>
+        <Form layout="vertical">
+          <div className=" mx-auto my-8 w-10/12 max-w-[23rem]">
+            <Form.Item
+              name="username"
+              label="User Name"
+              className="font-semibold"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input size="large"></Input>
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              className="font-semibold"
+              rules={[{ required: true, message: 'Please input your Password' }]}
+            >
+              <Input.Password size="large"></Input.Password>
+            </Form.Item>
+          </div>
+          <Form.Item className="mx-auto  w-3/12 min-w-[10rem] rounded-md border-2 border-primary-blue-700 text-center text-primary-blue-700 hover:border-primary-orange hover:text-primary-orange">
+            <button className="h-full w-full">Login</button>
+          </Form.Item>
+        </Form>
+        <p className="text-center text-xl font-medium">OR</p>
+        <button
+          className="mx-auto my-8 flex min-w-[16rem] flex-row items-center justify-center space-x-4 rounded-md border-2 border-red-700 py-2 text-red-700 lg:h-full"
+          onClick={handleSignInWithGoogle}
+        >
+          <GoogleOutlined className="text-3xl" />
+          <h1 className="text-xl">Sign in with Google</h1>
+        </button>
+      </div>
     </div>
   )
 }
