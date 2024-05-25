5fd117cca7519fd5333bec7cfcef176c245e4579
Thanaphol <thanaphol.iqtha@gmail.com>
2024-05-25 23:59:56 +0700

    feat: promotion more dialog

--- src/pages/Admin/Promotion/PromotionManagement.tsx
+++ src/pages/Admin/Promotion/PromotionManagement.tsx
@@ -1,14 +1,44 @@
+import CustomModal from '@/components/Modal/CustomModal'
 import FormatDate from '@/components/utils/formatDate'
 import { ADD_PROMOTIONADMIN_PATH } from '@/configs/route'
 import { IPromotionPrice } from '@/interfaces/PromotionPrice'
 import { AxiosInstance } from '@/lib/axios'
 import { PlusOutlined } from '@ant-design/icons'
-import { Button, Table, TableColumnsType } from 'antd'
+import { Button, DatePicker, Form, GetProps, Input, Table, TableColumnsType } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import dayjs from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import localeData from 'dayjs/plugin/localeData'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekYear from 'dayjs/plugin/weekYear'
+import weekday from 'dayjs/plugin/weekday'
+
+const { RangePicker } = DatePicker
+
+type RangePickerProps = GetProps<typeof DatePicker.RangePicker>
+dayjs.extend(customParseFormat)
+dayjs.extend(advancedFormat)
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
+const dateFormat = 'DD/MM/YYYY'
+const disabledDate: RangePickerProps['disabledDate'] = (current) => {
+  return current && current < dayjs().endOf('day')
+}
+
 const PromotionManagement: React.FC = () => {
   const [data, setData] = useState<IPromotionPrice[]>([])
+  const [visible, setVisible] = useState(false)
+  const [modalData, setModalData] = useState<IPromotionPrice | null>(null)
+  const [form] = Form.useForm()
+  const openModal = (data: IPromotionPrice) => {
+    setModalData(data)
+    setVisible(true)
+  }
 
   const column_data: TableColumnsType<IPromotionPrice> = [
     {
@@ -38,6 +68,13 @@ const PromotionManagement: React.FC = () => {
       key: 'startdate',
       render: (_: string, row: IPromotionPrice) => {
         return <p className="text-center">{FormatDate(row.endDate)}</p>
+      }
+    },
+    {
+      title: 'Detail',
+      key: 'manage',
+      render: (_: string, row: IPromotionPrice) => {
+        return <Button onClick={() => openModal(row)}>Detail</Button>
       }
     }
   ]
@@ -72,6 +109,66 @@ const PromotionManagement: React.FC = () => {
           </div>
         </div>
       </div>
+      <CustomModal
+        visible={visible}
+        title="Promotion Detail"
+        onClose={() => setVisible(false)}
+        footer={<Button onClick={() => setVisible(false)}>Close</Button>}
+      >
+        <Form layout="vertical" form={form}>
+          <div className="mx-auto grid grid-cols-1 gap-x-4 lg:grid-cols-3 ">
+            <Form.Item
+              label={<p className="text-sm font-semibold">ชื่อ</p>}
+              rules={[{ required: true, message: 'กรุณากรอกชื่อ' }]}
+            >
+              <Input size="large" value={modalData?.name} readOnly />
+            </Form.Item>
+            <Form.Item
+              label={<p className="text-sm font-semibold">ช่วงเวลาโปรมาชั่น</p>}
+              rules={[{ required: true, message: 'กรุณาเลือกช่วงโปรโมชั่น' }]}
+            >
+              <RangePicker
+                disabled
+                disabledDate={disabledDate}
+                format={dateFormat}
+                className="w-full !cursor-default !bg-white !text-black "
+                size="large"
+                value={[dayjs(modalData?.startDate), dayjs(modalData?.endDate)]}
+                inputReadOnly={true}
+                onClick={() => {
+                  return
+                }}
+              />
+            </Form.Item>
+            <Form.Item
+              label={<p className="text-sm font-semibold">ราคา</p>}
+              rules={[
+                { required: true, message: 'กรุณากรอกราคาโปรโมชั่น' },
+                { message: 'กรุณากรอกตัวเลข', pattern: /^[0-9]*$/ }
+              ]}
+            >
+              <Input size="large" value={`฿ ${modalData?.price}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} />
+            </Form.Item>
+            <div className="lg:col-span-3">
+              {modalData &&
+                modalData.roomTypePromotionPrice &&
+                modalData.roomTypePromotionPrice.map((room) => {
+                  return (
+                    <div key={room.roomType?.id} className="grid grid-cols-12">
+                      <Form.Item
+                        label={<p className="text-sm font-semibold">ประเภทห้อง</p>}
+                        className={'col-span-12'}
+                        rules={[{ required: true, message: 'กรุณาเลือกประเภทห้อง' }]}
+                      >
+                        <Input size="large" value={room.roomType?.name} readOnly />
+                      </Form.Item>
+                    </div>
+                  )
+                })}
+            </div>
+          </div>
+        </Form>
+      </CustomModal>
     </React.Fragment>
   )
 }
