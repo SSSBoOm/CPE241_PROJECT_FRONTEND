843d8b47bdcfc9080504cad0f1afc3b829359db2
Thanaphol <thanaphol.iqtha@gmail.com>
2024-05-25 18:57:30 +0700

    feat: history page

--- src/components/Card/ServiceCardHist.tsx
+++ src/components/Card/ServiceCardHist.tsx
@@ -1,42 +1,30 @@
 import { IReservation } from '@/interfaces/Reservation'
-import { FullscreenOutlined, UserOutlined } from '@ant-design/icons'
 
 type Props = {
   data: IReservation
 }
 
-const RoomCardHist = (props: Props) => {
+const ServiceCardHist = (props: Props) => {
   return (
     <>
       <div className="grid min-h-32 w-full min-w-[300px] rounded-md border-2 border-primary-blue-700 md:flex">
         <div className="w-full rounded-md bg-cover md:w-3/12">
           <img
             className="h-full content-center object-cover"
-            src={`https://evquseshrfnvyndhterj.supabase.co/storage/v1/object/public/cpe241-image/${props?.data?.room?.roomType?.imageUrl || ''}`}
+            src={`https://evquseshrfnvyndhterj.supabase.co/storage/v1/object/public/cpe241-image/${props?.data?.service?.serviceType?.imageUrl || ''}`}
             onError={(e) => {
               e.currentTarget.src = `${window.location.origin}/user_not_found.svg`
             }}
             alt="img"
           />
         </div>
-        <div className="w-full p-2 md:w-9/12">
-          <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="h-full w-full p-2 md:w-9/12">
+          <div className="grid h-full grid-cols-1 md:grid-cols-3">
             <div className="flex-1 flex-col flex-wrap ">
               <div className="mb-4 text-wrap">
-                <p className="text-3xl font-semibold">{props.data?.room?.roomType?.name}</p>
-                <p className="truncate text-sm">{props.data?.room?.roomType?.detail}</p>
+                <p className="text-3xl font-semibold">{props.data?.service?.name}</p>
+                <p className="truncate text-sm">{props.data?.service?.description}</p>
               </div>
-              <p className="text-sm md:text-lg">
-                <FullscreenOutlined />
-                Size: {props.data?.room?.roomType?.size || 10} m²
-              </p>
-              <p className="text-sm md:text-lg">
-                <UserOutlined />
-                Capacity: {props.data?.room?.roomType?.accommodate || 1}
-              </p>
-              {props.data?.room?.roomType?.bed && (
-                <p className="text-sm md:text-lg">Bed : {props.data?.room?.roomType?.bed}</p>
-              )}
             </div>
 
             <div className="flex-col flex-wrap content-center text-center md:flex-1">
@@ -60,4 +48,4 @@ const RoomCardHist = (props: Props) => {
   )
 }
 
-export default RoomCardHist
+export default ServiceCardHist
--- src/pages/History/HistoryPage.tsx
+++ src/pages/History/HistoryPage.tsx
@@ -1,3 +1,4 @@
+import ServiceCardHist from '@/components/Card/ServiceCardHist'
 import { IReservation } from '@/interfaces/Reservation'
 import { ReservationType } from '@/interfaces/enums/ReservationType'
 import { AxiosInstance } from '@/lib/axios'
@@ -27,66 +28,42 @@ const HistoryPage: React.FC = () => {
         <div className="container mx-auto rounded-md border-2 bg-white p-4">
           <h1 className="text-center text-3xl font-bold text-primary-blue-600">History Booking</h1>
         </div>
-        <div className="container col-span-2 col-start-1 mx-auto grid-cols-2 rounded-md border-2 bg-white text-primary-blue-600 lg:col-start-2">
+        {historyRoom.length === 0 && historyService.length === 0 && (
+          <div className="container mx-auto rounded-md border-2 bg-white p-4">
+            <h1 className="text-center text-2xl font-bold text-primary-blue-600">No History Found</h1>
+          </div>
+        )}
+        <div className="container col-span-2 col-start-1 mx-auto grid-cols-2 rounded-md border-2 bg-white p-4 text-primary-blue-600 lg:col-start-2">
           <div
             className={`col-span-2 col-start-1 m-5 text-4xl font-bold text-primary-blue-600 lg:col-start-2 ${historyRoom.length === 0 && 'hidden'}`}
           >
             Room
           </div>
           {/* if no historyRoom and historyservie */}
-          {historyRoom.length === 0 && historyService.length === 0 && (
-            <div className="container mx-auto rounded-md border-2 bg-white p-4">
-              <h1 className="text-center text-2xl font-bold text-primary-blue-600">No History Found</h1>
-            </div>
-          )}
-          {historyRoom
-            .sort((a, b) => {
-              return new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
-            })
-            .map((element, index) => {
-              return (
-                <div
-                  className=" col-span-2 col-start-1 m-5 place-items-center  content-center items-center justify-center place-self-center self-center justify-self-center object-center lg:col-span-4 lg:col-start-1"
-                  key={index}
-                >
-                  <RoomCardHist
-                    name={element.room?.roomType?.name || ''}
-                    content={element.room?.roomType?.detail || ''}
-                    size={element.room?.roomType?.detail || ''}
-                    accommodate={element.room?.roomType?.accommodate || 0}
-                    price={element.price}
-                    checkIn={element.startDate}
-                    checkOut={element.endDate}
-                    // image={element.room?.roomType?.image || ''}
-                  />
-                </div>
-              )
-            })}
+
+          <div className="container grid grid-cols-1 gap-y-6 px-4 md:mx-auto">
+            {historyRoom
+              .sort((a, b) => {
+                return new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
+              })
+              .map((element, index) => {
+                return <RoomCardHist data={element} key={index} />
+              })}
+          </div>
           <div
             className={`col-span-2 col-start-1 m-5 text-4xl font-bold text-primary-blue-600 lg:col-start-2 ${historyService.length === 0 && 'hidden'}`}
           >
             Service
           </div>
-          {historyService
-            .sort((a, b) => {
-              return new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
-            })
-            .map((element, index) => {
-              return (
-                <div
-                  className=" col-span-2 col-start-1 m-5 place-items-center  content-center items-center justify-center place-self-center self-center justify-self-center object-center lg:col-span-4 lg:col-start-1"
-                  key={index}
-                >
-                  <RoomCardHist
-                    name={element.service?.name || ''}
-                    content={element.service?.description || ''}
-                    price={element.price}
-                    checkIn={element.startDate}
-                    checkOut={element.endDate}
-                  />
-                </div>
-              )
-            })}
+          <div className="container grid grid-cols-1 gap-y-6 px-4 md:mx-auto">
+            {historyService
+              .sort((a, b) => {
+                return new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
+              })
+              .map((element, index) => {
+                return <ServiceCardHist data={element} key={index} />
+              })}
+          </div>
         </div>
       </div>
     </Fragment>
